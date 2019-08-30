e4d62dbfa5d79e70c6f94e6f489d43950b63a005
Yax <47990514+tw1sterz@users.noreply.github.com>
2019-08-30 14:00:57 +0200

    Update 1_word_shaker.cpp
    update 1.1 : 
     - Adding a word dictionary to play alone 
    - You can define a maximum attempts for a game
    - You can now replay without restarting the program

--- 1_word_shaker.cpp
+++ 1_word_shaker.cpp
@@ -26,36 +26,130 @@ string wordRandomizer(string word)
 
 int main()
 {
-    cout << "Welcome in the word shaker !" << endl;
-    cout << "Enter something to randomize : ";
-    string userWord;
-    cin >> userWord;
-    string randWord;
-    randWord = wordRandomizer(userWord);
-
-    string attemp;
-    bool isWin(false);
-    int gameTurn(1);
-    //main loop to check answer from user
-    do
+    srand(time(0));
+    string replay("O");
+    while(replay == "O")
     {
-        string attemp;
-        cout << "This is your shaked word : " << randWord << endl;
-        cin >> attemp;
-        //condition to check if user win
-        if(attemp == userWord)
+        cout << "Welcome in the word shaker !" << endl;
+
+        string isAlone("O");
+        cout << "Play alone ? (if you choose \"O\" a word will be randomly choose in a dictionary) O/N : " << endl;
+        cin >> isAlone;
+
+        string plainWord;
+        if(isAlone == "O")
         {
-            isWin = true;
+            ifstream dictionary("PATH");
+            if(dictionary)
+            {
+                int randLine(0);
+                randLine = rand() % 323577;
+                string computerWord("");
+                dictionary.seekg(randLine);
+                getline(dictionary, computerWord);
+                cout << computerWord << endl;
+                plainWord = computerWord;
+
+            }
         }
         else
         {
-            isWin = false;
-            gameTurn++;
+            cout << "Enter something to randomize : ";
+            cin >> plainWord;
         }
+        //define the variable of shacked word and applying the shaker on the word
+        string randWord;
+        randWord = wordRandomizer(plainWord);
 
-    }while(!isWin);
+        int chance(0);
+        cout << "Enter a number of attempts (type 999 is you don't want that) : ";
+        cin >> chance;
+        string userAttempt;
 
-    cout << "YOU  W I N in " << gameTurn << "attemp(s)";
+        bool isWin(false);
+        int gameTurn(1);
+        //main loop to check answer from user
+        do
+        {
+            if(chance == 0)
+            {
+                cout << "You haven't more try. The word was :  " << plainWord << endl;
+                cout << "You lose, play again ? : ";
+                cin >> replay;
+                break;
+            }
+
+            if(chance == 999)
+            {
+                cout << "This is your shacked word : " << randWord <<endl;
+                cin >> userAttempt;
+                if(userAttempt == plainWord)
+                {
+                    isWin = true;
+                    cout << "YOU  W I N in " << gameTurn << "attempt(s)" << endl;
+                    cout << "Do you want to play again ? O/N : ";
+                    cin >> replay;
+                    break;
+                }
+
+                else
+                {
+                    //set the boolean to false, check if user have activate attempt counting and decrement the variable relative to the chances
+                    isWin = false;
+                    gameTurn++;
+
+                    if(chance != 999)
+                    {
+                        chance--;
+                    }
+                }
+            }
+            cout << "This is your shacked word : " << randWord << " | You've " << chance << "chances" <<endl;
+            cin >> userAttempt;
+
+               if(userAttempt == plainWord)
+                {
+                    isWin = true;
+                    cout << "YOU  W I N in " << gameTurn << "attempt(s)" << endl;
+                    cout << "Do you want to play again ? O/N : ";
+                    cin >> replay;
+                    break;
+                }
+
+                else
+                {
+                    //set the boolean to false, check if user have activate attempt counting and decrement the variable relative to the chances
+                    isWin = false;
+                    gameTurn++;
+
+                    if(chance != 999)
+                    {
+                        chance--;
+                    }
+                }
+            }
+            //condition to check if user win
+            if(userAttempt == plainWord)
+            {
+                isWin = true;
+                cout << "YOU  W I N in " << gameTurn << "attempt(s)" << endl;
+                cout << "Do you want to play again ? O/N : ";
+                cin >> replay;
+                break;
+            }
+            else
+            {
+                //set the boolean to false, check if user have activate attempt counting and decrement the variable relative to the chances
+                isWin = false;
+                gameTurn++;
+                if(chance != 999)
+                {
+                    chance--;
+                }
+            }
+
+        }while(!isWin);
+    }
 
     return 0;
 }
